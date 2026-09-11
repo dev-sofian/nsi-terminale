5310aa52b0f0d3678e4dc4601b3b505e0c797a6d
Sofian <sofian.elba0@gmail.com>
2026-09-11 08:21:23 +0200

    COMMIT CALCULATRICE DNJNDIQKDNJDNIQNDJIQDNIQNDJQNDIQNDIJQDNIDQIDNZJDNQIDJQNDJIDQNDIJQNDIQNDIQJD

--- calculatrice.py
+++ calculatrice.py
@@ -1,3 +1,5 @@
+from math import sqrt
+
 def addition(n1, n2):
     return print(n1 + n2)
 
@@ -15,8 +17,25 @@ def division(n1, n2):
 def puissance(n1, n2):
     return print(n1**n2)
 
-def calculatrice(n1, n2):
-    entree = (input("Que souhaitez vous faire ? (Add, Sub, Mult, Div, Pow, Exit) : "))
+def racine_carree(n1):
+    return print(sqrt(n1))
+
+def calculatrice():
+    """
+    Fonction simulant une calculatrice. Les opérations possibles sont les suivantes : 
+    a+b, a-b, a*b, a/b (b non nul), a**b, sqrt(a)
+    @return : Lorsque entree == "Exit".
+    """
+    n1, n2 = 0, 0
+    entree = (input("Que souhaitez vous faire ? (Add, Sub, Mult, Div, Pow, Sqrt, Exit) : "))
+    if entree == "Sqrt":
+        n1 = int(input("Entrez nouveau nombre n°1 : "))
+        racine_carree(n1)
+    elif entree == "Exit":
+        return
+    else:
+        n1 = int(input("Entrez nouveau nombre n°1 : "))
+        n2 = int(input("Entrez nouveau nombre n°2 : "))
     if entree == "Add":
         addition(n1, n2)
     elif entree == "Sub":
@@ -27,10 +46,7 @@ def calculatrice(n1, n2):
         division(n1, n2)
     elif entree == "Pow":
         puissance(n1, n2)
-    elif entree == "Exit":
-        return
-    no1 = int(input("Entrez nouveau nombre n°1 : "))
-    no2 = int(input("Entrez nouveau nombre n°2 : "))
-    calculatrice(no1, no2)
 
-calculatrice(100, 20)
+    calculatrice()
+
+calculatrice()
